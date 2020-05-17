55dd1b45a99fca5ba6dff2a4c60a31325ee41c78
Souvik Biswas <sbis1999@gmail.com>
2020-05-17 14:32:02 +0530

    Add Docs to wear_mode

--- lib/wear_mode.dart
+++ lib/wear_mode.dart
@@ -9,9 +9,35 @@ typedef Widget WearModeBuilder(
 );
 
 class WearMode extends StatefulWidget {
+  /// This callback should contain two parameters:
+  ///
+  /// * [BuildContext] context
+  /// * [Mode] mode
   final WearModeBuilder builder;
+
+  /// A function that is called every time the watch triggers an
+  /// ambient update request.
+  ///
+  /// NOTE: If an update function is passed in, this widget will
+  /// not perform an update itself.
   final Function update;
 
+  /// Helps in determining the current mode of the wear device.
+  ///
+  /// It has a required parameter [builder], which is of
+  /// type [WearModeBuilder].
+  ///
+  /// The builder callback should contain two parameters:
+  ///
+  /// * [BuildContext] context
+  /// * [Mode] mode
+  ///
+  /// There is also an optional parameter [update], which takes a
+  /// function that is called every time the watch triggers an
+  /// ambient update request.
+  ///
+  /// NOTE: If an update function is passed in, this widget will
+  /// not perform an update itself.
   WearMode({
     @required this.builder,
     Key key,
@@ -24,13 +50,14 @@ class WearMode extends StatefulWidget {
 }
 
 class _WearModeState extends State<WearMode> {
-  /// Initialize ambient mode to be active
+  // Initialize current mode to be active
   Mode _mode = Mode.active;
 
   @override
   initState() {
     super.initState();
 
+    // Handles the rebuild of the widgets as the mode changes
     FlutterWear.channel.setMethodCallHandler((call) async {
       switch (call.method) {
         case 'ambientMode':
